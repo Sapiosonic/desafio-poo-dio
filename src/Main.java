69a671c4d81b898f3352ab71bfe07e3d4443f265
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-08-15 20:00:12 -0300

    final commit.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -24,8 +22,41 @@ public class Main {
         /*polimorfismo
         Conteudo conteudo = new Curso();*/
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRada = new Dev();
+        devRada.setNome("Radamés");
+        devRada.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Radamés" + devRada.getConteudosInscritos());
+        devRada.progredir();
+        devRada.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Radamés" + devRada.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Radamés" + devRada.getConteudosConcluidos());
+        System.out.println("XP: "+devRada.calcularTotalXp());
+
+        System.out.println("-----------------");
+
+        Dev devRamon = new Dev();
+        devRamon.setNome("Ramon");
+        devRamon.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Ramon" + devRamon.getConteudosInscritos());
+        devRamon.progredir();
+        devRamon.progredir();
+        devRamon.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Ramon" + devRamon.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Ramon" + devRamon.getConteudosConcluidos());
+        System.out.println("XP: "+devRamon.calcularTotalXp());
+
     }
 }
